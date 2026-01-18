ef4e9fd428e42e78be21db9c461de7a8de6e2eff
Michael Corrigan <mcorrigan89@gmail.com>
2026-01-17 23:09:17 -0500

    less logging

--- templates/logger/src/index.ts
+++ templates/logger/src/index.ts
@@ -1,19 +1,19 @@
-import pino from 'pino'
-import pinoPretty from 'pino-pretty'
+import pino from 'pino';
+import pinoPretty from 'pino-pretty';
 
-const isDevelopment = process.env.NODE_ENV !== 'production'
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const prettyStream = pinoPretty({
   colorize: isDevelopment,
   translateTime: 'yyyy-mm-dd HH:MM:ss.l o',
-  ignore: 'pid,hostname',
-})
+  ignore: 'pid,hostname,req,rpc',
+});
 
 export const logger = pino(
   {
     level: process.env.LOG_LEVEL || 'info',
   },
-  isDevelopment ? prettyStream : undefined,
-)
+  isDevelopment ? prettyStream : undefined
+);
 
-export type Logger = pino.Logger
+export type Logger = pino.Logger;
